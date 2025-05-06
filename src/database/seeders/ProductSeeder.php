70b2aba04b9918eb8f076e71683348a64480ca49
SHIVA_YUYA <nikyaku405@gmail.com>
2025-05-07 00:05:47 +0900

    mysqlに変換

--- src/database/seeders/ProductSeeder.php
+++ src/database/seeders/ProductSeeder.php
@@ -15,14 +15,12 @@ class ProductSeeder extends Seeder
                 'product_name' => '焼きそば',
                 'price' => 300,
                 'picture' => null,
-                'store_name' => '模擬店A', // 追加（Storeテーブルと一致させること）
             ],
             [
                 'product_code' => 'P002',
                 'product_name' => 'クレープ（いちご）',
                 'price' => 350,
                 'picture' => null,
-                'store_name' => 'クレープ屋', // 追加
             ],
         ]);
     }
